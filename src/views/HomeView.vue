70f5c06fecbc0bcc2fd9fd62866db930122eabce
rizheez <rezamuhammad980@gmail.com>
2025-08-04 14:28:39 +0800

    Refactor HomeView to improve structure with scroll spy functionality and organized section layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-import HomeSection from '@/components/HomeSection.vue';
-import AboutSection from '@/components/AboutSection.vue';
-import ProjectSection from '@/components/ProjectSection.vue';
-import ContactSection from '@/components/ContactSection.vue';
+import TheWelcome from "../components/TheWelcome.vue";
+import HomeSection from "@/components/HomeSection.vue";
+import AboutSection from "@/components/AboutSection.vue";
+import ProjectSection from "@/components/ProjectSection.vue";
+import ContactSection from "@/components/ContactSection.vue";
+import AOS from "aos";
+import { onMounted } from "vue";
 
+onMounted(() => {
+  AOS.init({
+    duration: 600,
+    once: true,
+  });
+});
 </script>
 
 <template>
   <main>
     <!-- <TheWelcome /> -->
-    <HomeSection />
-    <AboutSection />
-    <ProjectSection />
-    <ContactSection />
+    <div v-scroll-spy>
+      <section id="home">
+        <HomeSection />
+      </section>
+      <section id="about">
+        <AboutSection />
+      </section>
+      <section id="projects">
+        <ProjectSection />
+      </section>
+      <section id="contact">
+        <ContactSection />
+      </section>
+    </div>
   </main>
 </template>
